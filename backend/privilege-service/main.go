157e69f5165798373b0960d8ce4fa90a5d6319d8
MSI\Vlad <l4paykas@yandex.ru>
2023-11-12 20:36:45 +0300

    add rabbitmq

--- backend/privilege-service/main.go
+++ backend/privilege-service/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -11,43 +13,94 @@ import (
 	PS_DA "github.com/lapayka/rsoi-2/privilege-service/da"
 	PS_structs "github.com/lapayka/rsoi-2/privilege-service/structs"
 	TS_structs "github.com/lapayka/rsoi-2/tickect_service/structs"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 type GateWay struct {
 	db *PS_DA.DB
 	//logger *slog.Logger
+
+	rabbit_conn  *amqp.Connection
+	rabbit_chan  *amqp.Channel
+	rabbit_queue amqp.Queue
 }
 
 func main() {
 	router := mux.NewRouter()
 
 	db, _ := PS_DA.New("localhost", "postgres", "bonus", "1234")
-	gw := GateWay{db}
+
+	// Rabbit init
+	// -------------------------------------------------------------------
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		Logger.GetLogger().Print(err)
+		defer conn.Close()
+		return
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		Logger.GetLogger().Print(err)
+		defer ch.Close()
+		return
+	}
+
+	q, err := ch.QueueDeclare("tickets", false, false, false, false, nil)
+	if err != nil {
+		Logger.GetLogger().Print(err)
+		return
+	}
+	// -------------------------------------------------------------------
+
+	gw := GateWay{db: db,
+		rabbit_conn:  conn,
+		rabbit_chan:  ch,
+		rabbit_queue: q}
 
 	router.HandleFunc("/manage/health", http_utils.HealthCkeck).Methods("Get")
 	router.HandleFunc("/api/v1/me", gw.getPrivilegeAndHistory).Methods("Get")
 	router.HandleFunc("/api/v1/tickets", gw.getHistory).Methods("Get")
 	router.HandleFunc("/api/v1/tickets", gw.buyTicket).Methods("Post")
-	router.HandleFunc("/api/v1/tickets/{ticketUid}", gw.deleteTicket).Methods("DELETE")
+	//router.HandleFunc("/api/v1/tickets/{ticketUid}", gw.deleteTicket).Methods("DELETE")
 
-	err := http.ListenAndServe(":8050", router)
+	gw.deleteTicket()
+
+	err = http.ListenAndServe(":8050", router)
 	if err != nil {
 		//gw.logger.Error("failed to run app", "error", err)
 	}
 }
 
-func (gw *GateWay) deleteTicket(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	ticketUid := vars["ticketUid"]
+func (gw *GateWay) deleteTicket() {
+	fmt.Println("-->deleteTicket")
 
-	buy_ticket_info := TS_structs.Ticket{}
-	http_utils.ReadSerializable(r, &buy_ticket_info)
-
-	err := gw.db.DeleteTicket(ticketUid, buy_ticket_info.Price)
+	msgs, err := gw.rabbit_chan.Consume(gw.rabbit_queue.Name, "", true, false, false, false, nil)
 	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		return
+		Logger.GetLogger().Print(err)
 	}
+
+	fmt.Println("Consume")
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			ticket := TS_structs.Ticket{}
+			_ = json.Unmarshal(d.Body, &ticket)
+
+			fmt.Println(string(d.Body))
+
+			err := gw.db.DeleteTicket(ticket.TicketUid, ticket.Price)
+			if err != nil {
+				Logger.GetLogger().Println(err)
+				return
+			}
+		}
+	}()
+
+	<-forever
 }
 
 func (gw *GateWay) buyTicket(w http.ResponseWriter, r *http.Request) {
